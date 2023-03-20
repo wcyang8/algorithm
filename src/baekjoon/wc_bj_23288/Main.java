ce675335e29d86c13da1b3ac3898d060fa297e6a
unknown <wcyang7@mail.hongik.ac.kr>
2023-03-20 23:22:41 +0900

    123

--- src/baekjoon/wc_bj_23288/Main.java
+++ src/baekjoon/wc_bj_23288/Main.java
@@ -1,9 +1,22 @@
 package baekjoon.wc_bj_23288;
 
+/**
+ * 요약
+ * 1. 크기가 N x M인 지도
+ * 2. 지도 오른쪽 = 동쪽, 위쪽은 북쪽
+ * 3. 지도 좌표 (r,c)에서 r은 북쪽으로부터 떨어진 칸의 개수
+ * 4. c는 서쪽으로부터 떨어진 칸의 개수
+ * 5. 방향은 동쪽으로 시작
+ * 
+ * 
+ * 
+ * @author 양우철
+ *
+ */
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
