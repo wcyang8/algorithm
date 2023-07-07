ed830b48d49dfdb324eb17a69100a5e8da617655
wcyang8 <wcyang8@naver.com>
2023-07-07 17:54:15 +0900

    123

---
+++ src/baekjoon/wc_bj_2110/Main.java
@@ -0,0 +1,15 @@
+package baekjoon.wc_bj_2110;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+    }
+}
