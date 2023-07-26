4bf6704920d9cf974dacdce798945c041666ea1f
wcyang8 <wcyang8@naver.com>
2023-07-26 18:27:32 +0900

    bj 2482 start

---
+++ src/baekjoon/wc_bj_2482/Main.java
@@ -0,0 +1,12 @@
+package baekjoon.wc_bj_2482;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
