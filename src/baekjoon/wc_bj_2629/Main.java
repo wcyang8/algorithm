ffbd139c7ddeb7c10b456d17c958599259d7ad26
wcyang8 <wcyang8@naver.com>
2023-10-17 08:45:01 +0900

    bj 2629

--- src/baekjoon/wc_bj_2629/Main.java
+++ src/baekjoon/wc_bj_2629/Main.java
@@ -2,6 +2,7 @@ package baekjoon.wc_bj_2629;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 /**
  * 요약
@@ -10,8 +11,8 @@ import java.io.InputStreamReader;
  * 
  * 풀이
  * 1. 추의 무게로 dp
- * 2. (탐색한 추, 무게) = 가능 여부
- * 3. (탐색한 추, 무게) = (탐색한 추 - 1, 무게) or (탐색한 추 - 1, 무게 - 추무게)
+ * 2. (탐색한 추 번호, 무게) = 가능 여부
+ * 3. (탐색한 추 번호, 무게) = (탐색한 추 번호 - 1, 무게) or (탐색한 추 - 1, 무게 - 추무게)
  * 
  * @author SSAFY
  *
@@ -23,6 +24,43 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 
-		
+		int[] weight = new int[N];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int S = 0;
+		for(int i = 0; i < N; i++){
+			weight[i] = Integer.parseInt(st.nextToken());
+			S += weight[i];
+		}
+
+		boolean[][] dp = new boolean[N][S+1];
+
+		dp[0][0] = true;
+		dp[0][weight[0]] = true;
+		for(int i = 1; i < N; i++){
+			for(int j = 0; j <= S; j++){
+				dp[i][j] = dp[i-1][Math.abs(j - weight[i])] || dp[i-1][j];
+				if(j+weight[i] <= S) dp[i][j] = dp[i][j] || dp[i-1][j+weight[i]];
+			}
+		}
+
+		int M = Integer.parseInt(br.readLine());
+
+		StringBuilder sb = new StringBuilder();
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < M; i++){
+			boolean check = false;
+			int k = Integer.parseInt(st.nextToken());
+			if(k <= S) {
+				for (int j = 0; j < N; j++) {
+					if (dp[j][k]) {
+						check = true;
+						break;
+					}
+				}
+			}
+			sb.append(check?'Y':'N').append(' ');
+		}
+		System.out.println(sb);
 	}
 }
